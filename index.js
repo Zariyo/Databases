f42246d1b15c59258d7a1a174438820e7b27b8c3
Wiktorpluzek <75083416+Wiktorpluzek@users.noreply.github.com>
2022-01-19 11:44:38 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 const express = require('express');
 const app = express();
-const actors = require('./routes/actors');
-const movies = require('./routes/movies');
-require('dotenv').config();
+const cards = require('./routes/cards');
+const producents = require('./routes/producents')
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors())
+// „Podłączamy” obsługę „endpointów”, które zdefiniowaliśmy dla kolekcji 'cards' w katalogu routes/cards.js
+app.use('/cards', cards);
+app.use('/producents', producents)
 
+
+require('dotenv').config();
+const dbConnData = {
+    host: process.env.MONGO_HOST || '127.0.0.1',
+    port: process.env.MONGO_PORT || 27017,
+    database: process.env.MONGO_DATABASE || 'local'
+};
 // Łączymy się z bazą i „stawiamy” serwer API
-// Do kontaktu z serwerem Neo4J wykorzystamy bibliotekę neo4j-driver
-try {
-  require('./config/neo4jDriver');
+// Do kontaktu z serwerem MongoDB wykorzystamy bibliotekę Mongoose
 
-  app.use('/actors', actors);
-  app.use('/movies', movies);  
+const mongoose = require('mongoose');
 
-  console.log(`Connected to Neo4J.`)
-  const port = process.env.PORT || 5000
-  app.listen(port, () => {
-    console.log(`API server listening at http://localhost:${port}`);
-  });
-} catch(ex) {
-  console.error('Error connecting to Neo4J', ex);
+mongoose
+  .connect(`mongodb://${dbConnData.host}:${dbConnData.port}/${dbConnData.database}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(response => {
+    console.log(`Connected to MongoDB. Database name: "${response.connections[0].name}"`)
+    const port = process.env.PORT || 5000
+    app.listen(port, () => {
+      console.log(`API server listening at http://localhost:${port}`);
+    });
+  })
+  .catch(error => console.error('Error connecting to MongoDB', error));
 
-}
